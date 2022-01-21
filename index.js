ccf4d167a54fdf43550f3ff456c189f8bf6e38a1
risha-parveen <rishaparveen12@gmail.com>
2022-01-21 17:55:07 +0530

    updated

--- index.js
+++ index.js
@@ -4,20 +4,29 @@ const textArea=document.getElementsByClassName("text-area")
 const items=document.getElementsByClassName("items")
 const containerBox=document.getElementsByClassName("container-box")
 
+content={
+    "0":-1,
+    "1":-1,
+    "2":-1
+}
 
 for(let i=0;i<addBtn.length;i++){
     addBtn[i].addEventListener("click",()=>{
         if(textArea[i].value.trim().length!=0){
-            addCard(i)        }
+            addCard(i)        
+        }
     })
 }
 
 const addCard=(box_no)=>{
+    content[box_no]+=1
+
     const card=`
-        <div class="card">
+        <div class="card" id="card-${content[box_no]}">
             ${textArea[box_no].value}
         </div>
     `
     items[box_no].insertAdjacentHTML("beforebegin",card)
     textArea[box_no].value=""
+    console.log(content)
 }
