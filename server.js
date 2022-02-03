60ba8119aeba3ca2f49980dc095a98f9186c5f6b
risha-parveen <rishaparveen12@gmail.com>
2022-02-03 21:41:19 +0530

    updated file- works

--- server.js
+++ server.js
@@ -16,22 +16,102 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
 app.use(express.json())
 
+let result=null
+
 app.post('/save',async (req,res)=>{
-    let result=await Database.find({})
-    console.log(result)
-    if(result===[]){
-        console.log("not empty")
+    try{
+        result=await Database.find({}) 
+    }
+    catch(error){
+        res.status(500).send({
+            action:"failed to update",
+            success:false
+        })
+        return
+    }
+    if(result.length!==0){
+        switch(req.body.title){
+            case 'To Do':
+                result[0]['To Do'].push({
+                    cardId:req.body.cardId,
+                    description:req.body.description
+                })
+                break
+            case 'Doing':
+                result[0]['Doing'].push({
+                    cardId:req.body.cardId,
+                    description:req.body.description
+                })
+                break
+            case 'Done':
+                result[0]['Done'].push({
+                    cardId:req.body.cardId,
+                    description:req.body.description
+                })
+                break
+            default:
+                //nothing
+        }
+        try{
+            console.log(result)
+            await Database.replaceOne({id:'id'},result[0],{upsert:true}).then(result=>console.log(result))
+            
+            res.status(200).send({
+                action:"updated",
+                success:true
+            })
+        }catch(error){
+            console.log(error)
+            res.status(500).send({
+                action:"not updated",
+                success:false
+            })
+            return
+        }
     }
     else{
-        console.log("empty")
-        const db=new Database({
-            id:req.body.id,
-            'To Do':[req.body['To Do']],
-            'Doing':[req.body['Doing']],
-            'Done':[req.body['Done']]
-        })
-        console.log(db.Done)
-        await db.save()
+        let new_data={
+            id:'id',
+            'To Do':[],
+            'Doing':[],
+            'Done':[]
+        }
+        switch(req.body.title){
+            case 'To Do':
+                new_data['To Do'].push({
+                    cardId:req.body.cardId,
+                    description:req.body.description
+                })
+                break
+            case 'Doing':
+                new_data['Doing'].push({
+                    cardId:req.body.cardId,
+                    description:req.body.description
+                })
+                break
+            case 'Done':
+                new_data['Done'].push({
+                    cardId:req.body.cardId,
+                    description:req.body.description
+                })
+                break
+            default:
+                //nothing
+        }
+        try{
+            const db=new Database(new_data)
+            await db.save()
+            res.status(200).send({
+                action:"inserted",
+                success:true
+            })
+        }
+        catch(error){
+            res.status(500).send({
+                success:false
+            })
+        }
+        console.log(result)
     }
 })
 
