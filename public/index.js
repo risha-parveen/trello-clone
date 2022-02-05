b213576bcf8bc725235670d360843852d5a561f1
risha-parveen <rishaparveen12@gmail.com>
2022-02-05 22:58:47 +0530

    previous stored value rendered

--- public/index.js
+++ public/index.js
@@ -6,84 +6,132 @@ const containerBox=document.getElementsByClassName("container-box")
 const card=document.getElementsByClassName("card")
 const cardArea=document.getElementsByClassName("card-area")
 const deleteCardBtn=document.getElementsByClassName("delete-card")
-const cardCombo=document.getElementsByClassName("card-combo")
+const columnTitle=document.getElementsByClassName("column-title")
+
+let title,description=null 
+
+let content=null
 
 let dragItem=null
 
-for(let i=0;i<cardArea.length;i++){
-    cardArea[i].addEventListener('dragstart',dragStart,false)
-    cardArea[i].addEventListener('drag',drag,false)
-    cardArea[i].addEventListener('dragend',dragEnd,false)
-    cardArea[i].addEventListener('dragover',dragOver,false)
-    cardArea[i].addEventListener('dragenter',dragEnter,false)
-    cardArea[i].addEventListener('drop',Drop,false)
-}
+let startColumn,endColumn=null
 
-function dragStart(e){
-    dragItem=e.target
-    setTimeout(()=>{
-        e.target.display="none"
-    },0)
-    console.log(dragItem)
-}
+// drag and drop functionality=>
+for(let col=0;col<cardArea.length;col++){
+    cardArea[col].addEventListener('dragstart',e=>{
+        dragItem=e.target
+        setTimeout(()=>{
+            e.target.display="none"
+        },0)
+    })
 
-function drag(e){
-    setTimeout(()=>{
-        e.target.style.display="none"
-    },0)
-}
+    cardArea[col].addEventListener('drag',e=>{
+        setTimeout(()=>{
+            e.target.style.display="none"
+        },0)
+    })
 
-function dragEnd(e){
-    setTimeout(()=>{
-        e.target.style.display="block"
-    },0)
-    dragItem=null
+    cardArea[col].addEventListener('dragend',e=>{
+        setTimeout(()=>{
+            e.target.style.display="block"
+        },0)
+        dragItem=null
+    })
+
+    cardArea[col].addEventListener('dragover',e=>{
+        e.preventDefault()
+    })
+
+    cardArea[col].addEventListener('dragenter',e=>{
+        e.preventDefault()
+    })
+
+    cardArea[col].addEventListener('drop',e=>{
+        if(e.target.className==="card-drop"){
+            insertAfter(dragItem,e.target.parentNode)
+        }
+    })
 }
 
 function insertAfter(newNode, existingNode) {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
 
-function Drop(e){
-    if(e.target.className==="card-drop"){
-        insertAfter(dragItem,e.target.parentNode)
-    }
-}
+// added event listener for the button
 
-function dragOver(e){
-    e.preventDefault()
-}
-
-function dragEnter(e){
-    e.preventDefault()
-}
-
-for(let i=0;i<addBtn.length;i++){
-    addBtn[i].addEventListener("click",()=>{
-        if(textArea[i].value.trim().length!=0){
-            addCard(i)        
+for(let box_no=0; box_no<addBtn.length; box_no++){
+    addBtn[box_no].addEventListener("click",()=>{
+        if(textArea[box_no].value.trim().length!=0){
+            description=textArea[box_no].value
+            addCard(box_no,description)        
         }
     })
 }
 
-
-
-const addCard=(box_no)=>{
+const addCard=(box_no,description)=>{
     const cardnode=`
         <div class="card-combo" draggable="true">
             <div class="card">
-                ${textArea[box_no].value}
+                ${description}
                 <button class="delete-card">x</button>
             </div>
             <div class="card-drop"></div>
         </div>
     `
     cardArea[box_no].innerHTML+=cardnode
+
     textArea[box_no].value=""
 
+    addDeleteCardEventListener(box_no)
+}
+
+const addDeleteCardEventListener=box_no=>{
     for(let i=0;i<cardArea[box_no].children.length;i++){
+        if(cardArea[box_no].children[i].firstElementChild.firstElementChild===null) continue
         cardArea[box_no].children[i].firstElementChild.firstElementChild.addEventListener('dblclick',e=>{
             e.target.parentNode.parentNode.remove()
         })
     }
 }
+
+let id=null
+
+const renderData=(json)=>{
+    let data=json[0]
+    for(let i in data){
+        switch(i){
+            case "id":
+                id="id"
+                break
+            case "To Do":
+                title="To Do"
+                box_no=0
+                break
+            case "Doing":
+                title="Doing"
+                box_no=1
+                break
+            case "Done":
+                title="Done"
+                box_no=2
+                break
+            default:
+                continue
+        }
+        for(let j in data[title]){
+            description=data[title][j].description
+            cardId=data[title][j].cardId
+            addCard(box_no,description)
+        }
+    }
+}
+
+const getData = async ()=>{
+    const response = await fetch('/get_data')
+    const json=await response.json()
+    console.log(json)
+    if(json.length===0) return
+    renderData(json)
+}
+
+window.onload=getData
